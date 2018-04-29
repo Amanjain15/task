1418558758df81f4f8e13c24277c36d067242f93
Amanjain15 <amanjain.1596@gmail.com>
2018-04-29 05:36:53 +0530

    Changes in wsgi

--- fyle/settings.py
+++ fyle/settings.py
@@ -123,10 +123,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = "media/"
 # STATIC_ROOT='static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static/"),
    # '/var/www/static/',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
